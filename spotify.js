aeb400cb0a051793efe3c3dbc49dfecb562291a9
SalutarySide12 <105707412+SalutarySide12@users.noreply.github.com>
2022-11-20 09:29:17 +0530

    Add files via upload

---
+++ spotify.js
@@ -0,0 +1,12 @@
+let audioElement = new Audio('faded.mp3');
+let myProgressBar = document.getElementById('myProgressBar');
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+    }
